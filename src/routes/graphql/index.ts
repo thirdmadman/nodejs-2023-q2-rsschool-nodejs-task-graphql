46c0980d14b25e61f6faa5a8ea2739b45a662c99
thirdmadman <thirdmadman@gmail.com>
2023-07-19 19:14:54 +0200

    feat: add basic graphql

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,6 +1,35 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { graphql } from 'graphql';
+import { graphql, GraphQLObjectType, GraphQLSchema, GraphQLList, GraphQLNonNull } from 'graphql';
+import { UUIDType } from './types/uuid.js';
+import { postObjectType } from './typedefs/postObjectType.js';
+import { userObjectType } from './typedefs/userObjectType.js';
+import { getAllPosts, getPostById } from './resolvers/post/queries.js';
+import { getAllUsers } from './resolvers/user/queries.js';
+
+
+const queryType = new GraphQLObjectType({
+  name: "Query",
+  fields: {
+    post: {
+      type: postObjectType,
+      args: {
+        id: { type: new GraphQLNonNull(UUIDType) },
+      },
+      resolve: getPostById
+    },
+    posts: {
+      type: new GraphQLNonNull(new GraphQLList(postObjectType)),
+      resolve: getAllPosts,
+    },
+    users: {
+      type: new GraphQLNonNull(new GraphQLList(userObjectType)),
+      resolve: getAllUsers,
+    }
+  },
+})
+
+const schema = new GraphQLSchema({ query: queryType })
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   fastify.route({
@@ -13,7 +42,12 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
       },
     },
     async handler(req) {
-      return {};
+      return graphql({
+        schema,
+        source: req.body.query,
+        variableValues: req.body.variables,
+        rootValue: fastify
+      });
     },
   });
 };
