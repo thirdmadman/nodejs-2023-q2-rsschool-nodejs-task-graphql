fbc6537eabc006f32c86e06cad0ca5bbbd9fd84c
thirdmadman <thirdmadman@gmail.com>
2023-07-19 18:52:47 +0200

    feat: add userObjectType

---
+++ src/routes/graphql/typedefs/userObjectType.ts
@@ -0,0 +1,11 @@
+import { GraphQLInt, GraphQLNonNull, GraphQLObjectType, GraphQLString } from "graphql";
+import { UUIDType } from "../types/uuid.js";
+
+export const userObjectType = new GraphQLObjectType({
+  name: "User",
+  fields: () => ({
+    id: { type: new GraphQLNonNull(UUIDType) },
+    name: { type: new GraphQLNonNull(GraphQLString) },
+    balance: { type: new GraphQLNonNull(GraphQLInt) },
+  }),
+})
