4f381340ffc28598bd8ca9ac29f21e853b163368
thirdmadman <thirdmadman@gmail.com>
2023-07-20 10:07:47 +0200

    feat: add queries for memberType

---
+++ src/routes/graphql/resolvers/member-type/queries.ts
@@ -0,0 +1,34 @@
+import { GraphQLFieldConfig, GraphQLList, GraphQLNonNull, GraphQLString } from 'graphql';
+import { FastifyInstanceType } from '../../types/FastifyInstanceType.js';
+import { memberTypeObjectType } from '../../typedefs/memberTypeObjectType.js';
+
+export const getMemberTypeById = async (
+  fInstance: FastifyInstanceType,
+  { id }: { id: string },
+) => {
+  const { prisma } = fInstance;
+
+  return await prisma.memberType.findUnique({
+    where: {
+      id,
+    },
+  });
+};
+
+export const getAllMemberTypes = async (fInstance: FastifyInstanceType) => {
+  const { prisma } = fInstance;
+  return await prisma.memberType.findMany();
+};
+
+export const memberType: GraphQLFieldConfig<FastifyInstanceType, null, { id: string }> = {
+  type: memberTypeObjectType,
+  args: {
+    id: { type: new GraphQLNonNull(GraphQLString) },
+  },
+  resolve: getMemberTypeById,
+};
+
+export const memberTypes: GraphQLFieldConfig<FastifyInstanceType, null, null> = {
+  type: new GraphQLNonNull(new GraphQLList(memberTypeObjectType)),
+  resolve: getAllMemberTypes,
+};
