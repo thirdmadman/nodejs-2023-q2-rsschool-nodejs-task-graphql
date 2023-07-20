010b92cfa9825786f3fe8e6735880dcd8d389a80
thirdmadman <thirdmadman@gmail.com>
2023-07-20 09:55:57 +0200

    feat: add user query

--- src/routes/graphql/resolvers/query.ts
+++ src/routes/graphql/resolvers/query.ts
@@ -1,8 +1,9 @@
 import { post, posts } from './post/queries.js';
-import { users } from './user/queries.js';
+import { user, users } from './user/queries.js';
 
 export const query = {
   post,
   posts,
+  user,
   users,
 };
