2de70e5e1965c67401695e351fd244ffd3809716
thirdmadman <thirdmadman@gmail.com>
2023-07-20 10:28:10 +0200

    fix: correct name

--- src/routes/graphql/typedefs/memberTypeObjectType.ts
+++ src/routes/graphql/typedefs/memberTypeObjectType.ts
@@ -1,7 +1,7 @@
 import { GraphQLFloat, GraphQLInt, GraphQLNonNull, GraphQLObjectType, GraphQLString } from "graphql";
 
 export const memberTypeObjectType = new GraphQLObjectType({
-  name: "Post",
+  name: "MemberType",
   fields: () => ({
     id: { type: new GraphQLNonNull(GraphQLString) },
     discount: { type: new GraphQLNonNull(GraphQLFloat) },
