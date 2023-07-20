b610804982f5e1eeff750b36c9c8b911a54bca45
thirdmadman <thirdmadman@gmail.com>
2023-07-20 16:13:23 +0200

    feat: update user queries

--- src/routes/graphql/resolvers/user/queries.ts
+++ src/routes/graphql/resolvers/user/queries.ts
@@ -1,7 +1,7 @@
-import { GraphQLFieldConfig, GraphQLList, GraphQLNonNull } from "graphql";
-import { FastifyInstanceType } from "../../types/FastifyInstanceType.js";
-import { UUIDType } from "../../types/uuid.js";
-import { userObjectType } from "../../typedefs/userObjectType.js";
+import { GraphQLFieldConfig, GraphQLList, GraphQLNonNull } from 'graphql';
+import { FastifyInstanceType } from '../../types/FastifyInstanceType.js';
+import { UUIDType } from '../../types/uuid.js';
+import { userObjectType } from '../../typedefs/userObjectType.js';
 
 export const getAllUsers = async (fInstance: FastifyInstanceType) => {
   const { prisma } = fInstance;
@@ -14,12 +14,50 @@ export const getUserById = async (
 ) => {
   const { prisma } = fInstance;
 
-  return await prisma.user.findUnique({
+  const user = await prisma.user.findUnique({
     where: {
       id,
     },
   });
+
+  if (!user) {
+    return null;
+  }
+
+  const profile = await prisma.profile.findUnique({
+    where: {
+      userId: id,
+    },
+  });
+
+  if (!profile) {
+    return user;
+  }
+
+  const memberType = await prisma.memberType.findUnique({
+    where: {
+      id: profile.memberTypeId,
+    },
+  });
+
+  if (!memberType) {
+    return {...user, profile};
+  }
+
+  return {...user, profile: {...profile, memberType}};;
 };
+
+// export const getUserProfileByUserId = async (
+//   fInstance: FastifyInstanceType,
+//   { id }: { id: string },
+// ) => {
+//   const { prisma } = fInstance;
+//   return await prisma.profile.findUnique({
+//     where: {
+//       userId: id,
+//     },
+//   });
+// };
 
 export const users: GraphQLFieldConfig<FastifyInstanceType, null, null> = {
   type: new GraphQLNonNull(new GraphQLList(userObjectType)),
@@ -32,4 +70,4 @@ export const user: GraphQLFieldConfig<FastifyInstanceType, null, { id: string }>
     id: { type: new GraphQLNonNull(UUIDType) },
   },
   resolve: getUserById,
-}; 
+};
