a515445e068609b66ffdcda7941ebc4b9fe067ea
thirdmadman <thirdmadman@gmail.com>
2023-07-20 16:12:35 +0200

    feat: update User

--- src/routes/graphql/typedefs/userObjectType.ts
+++ src/routes/graphql/typedefs/userObjectType.ts
@@ -1,5 +1,6 @@
 import { GraphQLFloat, GraphQLNonNull, GraphQLObjectType, GraphQLString } from "graphql";
 import { UUIDType } from "../types/uuid.js";
+import { profileObjectType } from "./profileObjectType.js";
 
 export const userObjectType = new GraphQLObjectType({
   name: "User",
@@ -7,5 +8,6 @@ export const userObjectType = new GraphQLObjectType({
     id: { type: new GraphQLNonNull(UUIDType) },
     name: { type: new GraphQLNonNull(GraphQLString) },
     balance: { type: new GraphQLNonNull(GraphQLFloat) },
+    profile: { type: profileObjectType },
   }),
 })
