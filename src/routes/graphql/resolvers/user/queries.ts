e3b36164ff3cee9a7624f9fd9cf6560f2a009c1c
thirdmadman <thirdmadman@gmail.com>
2023-07-20 09:55:14 +0200

    fix: correct user queries

--- src/routes/graphql/resolvers/user/queries.ts
+++ src/routes/graphql/resolvers/user/queries.ts
@@ -1,13 +1,35 @@
 import { GraphQLFieldConfig, GraphQLList, GraphQLNonNull } from "graphql";
 import { FastifyInstanceType } from "../../types/FastifyInstanceType.js";
-import { postObjectType } from "../../typedefs/postObjectType.js";
+import { UUIDType } from "../../types/uuid.js";
+import { userObjectType } from "../../typedefs/userObjectType.js";
 
 export const getAllUsers = async (fInstance: FastifyInstanceType) => {
   const { prisma } = fInstance;
   return await prisma.user.findMany();
 };
 
+export const getUserById = async (
+  fInstance: FastifyInstanceType,
+  { id }: { id: string },
+) => {
+  const { prisma } = fInstance;
+
+  return await prisma.user.findUnique({
+    where: {
+      id,
+    },
+  });
+};
+
 export const users: GraphQLFieldConfig<FastifyInstanceType, null, null> = {
-  type: new GraphQLNonNull(new GraphQLList(postObjectType)),
+  type: new GraphQLNonNull(new GraphQLList(userObjectType)),
   resolve: getAllUsers,
 };
+
+export const user: GraphQLFieldConfig<FastifyInstanceType, null, { id: string }> = {
+  type: userObjectType,
+  args: {
+    id: { type: new GraphQLNonNull(UUIDType) },
+  },
+  resolve: getUserById,
+}; 
