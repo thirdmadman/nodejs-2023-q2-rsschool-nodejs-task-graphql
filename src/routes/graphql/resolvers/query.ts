78b883704080ce82c8c3d3def43915506ecce66a
thirdmadman <thirdmadman@gmail.com>
2023-07-20 12:52:16 +0200

    feat: impl use of profile queries

--- src/routes/graphql/resolvers/query.ts
+++ src/routes/graphql/resolvers/query.ts
@@ -1,5 +1,6 @@
 import { memberType, memberTypes } from './member-type/queries.js';
 import { post, posts } from './post/queries.js';
+import { profile, profiles } from './profile/queries.js';
 import { user, users } from './user/queries.js';
 
 export const query = {
@@ -9,4 +10,6 @@ export const query = {
   users,
   memberType,
   memberTypes,
+  profile,
+  profiles
 };
