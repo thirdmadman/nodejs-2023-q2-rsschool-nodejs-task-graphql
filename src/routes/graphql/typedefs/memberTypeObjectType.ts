18856902d6660cf1b132c93a16d92b8ddea67a15
thirdmadman <thirdmadman@gmail.com>
2023-07-20 12:50:42 +0200

    feat: impl use of memberTypeIdObjectType

--- src/routes/graphql/typedefs/memberTypeObjectType.ts
+++ src/routes/graphql/typedefs/memberTypeObjectType.ts
@@ -1,9 +1,17 @@
-import { GraphQLFloat, GraphQLInt, GraphQLNonNull, GraphQLObjectType, GraphQLString } from "graphql";
+import { GraphQLEnumType, GraphQLFloat, GraphQLInt, GraphQLNonNull, GraphQLObjectType } from "graphql";
+
+export const memberTypeIdObjectType = new GraphQLEnumType({
+  name: "MemberTypeId",
+  values: {
+    basic: { value: "basic" },
+    business: {value: "business" },
+  },
+})
 
 export const memberTypeObjectType = new GraphQLObjectType({
   name: "MemberType",
   fields: () => ({
-    id: { type: new GraphQLNonNull(GraphQLString) },
+    id: { type: new GraphQLNonNull(memberTypeIdObjectType) },
     discount: { type: new GraphQLNonNull(GraphQLFloat) },
     postsLimitPerMonth: { type: new GraphQLNonNull(GraphQLInt) },
   }),
